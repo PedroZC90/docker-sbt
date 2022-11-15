67c9d3436d38c839a6ad805b868d5534899f2f60
pedrozc90 <pedrozc90@gmail.com>
2022-11-15 16:22:18 -0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+docker image rm pedrozc90/sbt:test
+
 docker build --no-cache \
     --network host \
     --file ./Dockerfile \
     --tag pedrozc90/sbt:test \
     .
+
+# exit if docker build fails
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
 
 docker run -it \
     --rm \
